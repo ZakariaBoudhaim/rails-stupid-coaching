b042725d03ada205d66c0f96f825cfa9e0e1ccd6
Zakariaboudhaim <zakariaboudhaim@gmail.com>
2024-01-15 14:43:53 +0100

    stupid coach using rails

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,8 @@ class PagesController < ApplicationController
       @answer = 'Great'
     elsif params[:answer].include?('?')
       @answer = 'Silly question, get dressed and go to work!'
+    elsif params[:answer] == ""
+      @answer = "you've been so quiet lately! something fishy"
     else
       @answer = "I don't care, get dressed and go to work!"
     end
